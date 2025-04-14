cb34f15edcf6c5e8b5c27e6ff565625fb88edc67
Nour-Mahgoub <s-nourhan.mahgoub@zewailcity.edu.eg>
2025-04-14 10:42:09 +0000

    SLAM Simulation

---
+++ build/slam_gmapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/slam_project/src/slam_gmapping
+# Build directory: /root/slam_project/build/slam_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot3_simulations/turtlebot3_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/slam_project/src/turtlebot3_simulations/turtlebot3_gazebo
+# Build directory: /root/slam_project/build/turtlebot3_simulations/turtlebot3_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
